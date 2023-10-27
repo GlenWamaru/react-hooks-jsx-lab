9628952cf36682c248a9c4d89ea9057d4dbccb85
SengokuGarp <garpirungu@gmail.com>
2023-10-27 10:07:30 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { image } from "../data/data";
+// eslint-disable-next-line no-unused-vars
+import { name, city, image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Write something about yourself here.</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"/>
+
+    </div>
+
+  )
 }
 
 export default About;
